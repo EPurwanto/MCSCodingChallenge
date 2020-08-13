dd553a3d031151ba71eea5be39eeae949e9e2ba0
Edward Purwanto <edward.purwanto@students.mq.edu.au>
2020-08-13 14:03:50 +1000

    re-configured as ts project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <svg className="canvas">
+                <rect fill={"#FF00FF"} width={250} height={250}/>
+            </svg>
+        </div>
+    );
+}
+
+export default App;
